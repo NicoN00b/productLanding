5bcda369fda72c661d7ca3e8c5b50a00df9a91cb
nr <nicholas.raethke@gmail.com>
2017-09-22 18:13:07 -0700

    add new animal functionality.

--- angleZoo/app/app.component.ts
+++ angleZoo/app/app.component.ts
@@ -9,27 +9,8 @@ import { Animal } from './animal.model';
       <h3>{{month}}/{{day}}/{{year}}</h3>
 
       <animal-list [childAnimalList]="masterAnimalList" (clickSender)="editAnimal($event)"></animal-list>
-      <div *ngIf="selectedAnimal">
-        <h3>{{selectedAnimal.species}}{{selectedAnimal.name}}</h3>
-        <h3>Edit Info</h3>
-        <label>Enter Animal Species:</label>
-        <input [(ngModel)]="selectedAnimal.species">
-        <label>Enter Animal Name:</label>
-        <input [(ngModel)]="selectedAnimal.name">
-        <label>Enter Animal Age:</label>
-        <input [(ngModel)]="selectedAnimal.age">
-        <label>Enter Animal Diet:</label>
-        <input [(ngModel)]="selectedAnimal.diet">
-        <label>Enter Animal Location:</label>
-        <input [(ngModel)]="selectedAnimal.location">
-        <label>Enter Animal Sex:</label>
-        <input [(ngModel)]="selectedAnimal.sex">
-        <label>Enter Animal Likes:</label>
-        <input [(ngModel)]="selectedAnimal.likes">
-        <label>Enter Animal Dislikes:</label>
-        <input [(ngModel)]="selectedAnimal.dislikes">
-        <button (click)="finishedEditing()">Finish amp Save</button>
-      </div>
+      <edit-animal [childSelectedAnimal]="selectedAnimal" (doneButtonClickedSender)="finishedEditing"></edit-animal>
+      <new-animal (newAnimalSender)="addAnimal($event)"></new-animal>
     </div>
   `
 })
@@ -40,7 +21,6 @@ export class AppComponent {
   month: number = this.currentTime.getMonth() + 1;
   day: number = this.currentTime.getDate();
   year: number = this.currentTime.getFullYear();
-
   masterAnimalList: Animal[] = [
     new Animal("Elephant", "Mel", 35, "Herbivore", "Savannah Simulacra", "Male", "dreaming about Africa", "flashes and gawking yokels"),
     new Animal("Ocelet", "Isis", 12, "Carnivore", "Plastic Jungle", "Female", "stalking, torturing, feasting", "pretty much everyone and everything"),
@@ -54,5 +34,9 @@ export class AppComponent {
 
   editAnimal(clickedAnimal) {
    this.selectedAnimal = clickedAnimal;
+  }
+
+  addAnimal(newAnimalFromChild: Animal) {
+    this.masterAnimalList.push(newAnimalFromChild);
   }
 }
